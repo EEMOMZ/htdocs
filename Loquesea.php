59dcae29f6ff28d7f7cbec1fd0c7443092c71ae1
EEMOMZ <enrique.maldonadommm@gmail.com>
2025-05-16 15:53:05 +0200

    dw

--- Loquesea.php
+++ Loquesea.php
@@ -55,6 +55,7 @@ $result = $conn->query($sql);
 <!DOCTYPE html>
 <html lang="es">
 <head>
+    <link rel="stylesheet" href="estilo.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulario de Usuarios</title>
